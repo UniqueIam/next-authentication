77b03eee0e7f6bb2f689e64a3b10298e9b6b335b
Abhimanyu Kumar <abhimanyukumar83097@gmail.com>
2024-09-03 19:47:00 +0530

    user-model

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,5 +1,35 @@
 import mongoose from 'mongoose'
 
 const userSchema = new mongoose.Schema({
-    
+    username:{
+        type:String,
+        required:[true,'Please Provide the username'],
+        unique
+    },
+    email:{
+        type:String,
+        required:[true,'Please Provide an Email'],
+        unique
+    },
+    password:{
+        type:String,
+        required:true
+    },
+    isVerified:{
+        type:Boolean,
+        default:false
+    },
+    isAdmin:{
+        type:Boolean,
+        default:false
+    },
+    forgotPasswordToken:String,
+    forgotPasswordTokenExpiray:Date,
+    verifyToken:String,
+    verifyTokenExpiray:Date
+
 })
+
+const User = mongoose.models.users || mongoose.model("users",userSchema);
+
+export default User
